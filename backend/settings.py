a2e11cbf71f6b87e189e176a3d0978fffcddba1e
Abhay <162544830+aabhay007@users.noreply.github.com>
2024-11-14 21:09:46 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -115,5 +115,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'akshay17012@gmail.com'
-EMAIL_HOST_PASSWORD = 'tfsv jqsi ygrn cwog'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
